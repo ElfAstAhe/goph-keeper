f0cb6c1c8c03a1015f951d8a6f8a2ab6904c3c18
elf.ast.ahe <elf.ast.ahe@gmail.com>
2026-02-06 10:28:38 +0300

    dev: intermediate, bll, dal errors ...

--- internal/err/dal_not_found.go
+++ internal/err/dal_not_found.go
@@ -1,1 +1,31 @@
 package err
+
+import "fmt"
+
+// DalNotFoundError — отсутствие сущности
+type DalNotFoundError struct {
+    Entity string // Какая сущность (например, "User" или "UserData")
+    Value  string // Какое значение вызвало конфликт (например, "login 'admin'")
+    Err    error  // Исходная ошибка из драйвера БД (опционально)
+}
+
+func NewDalNot                                                                                                 FoundError(entity, value string, err error) *DalNotFoundError {
+    return &DalNotFoundError{
+        Entity: entity,
+        Value:  value,
+        Err:    err,
+    }
+}
+
+func (e *DalNotFoundError) Error() string {
+    msg := fmt.Sprintf("DAL: %s with value [%s] already exists", e.Entity, e.Value)
+    if e.Err != nil {
+        return fmt.Sprintf("%s: %v", msg, e.Err)
+    }
+
+    return msg
+}
+
+func (e *DalNotFoundError) Unwrap() error {
+    return e.Err
+}
